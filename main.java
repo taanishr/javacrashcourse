89180c6288e54a90f0c46689372cbec61f01074e
Swagenwagen <tannedswegion@gmail.com>
2022-08-29 17:53:24 -0700

    first commit

---
+++ main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Variables are like boxes; they're named and store values. This is a dumbed down explanation of course; the real one has to deal with references
+        int num = 1; // primitive type
+        double decimal = 0.1; // primitive type
+        String str = "str"; // reference type
+
+        x = System.in.read();
+        if (x < 10)
+
+        //System.out.println("test");
+
+    }
+}
